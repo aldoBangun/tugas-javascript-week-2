1cedefd42356aa0ea904a8fc7b5ddc81b7f98361
aldoBangun <punk.aldo68@gmail.com>
2022-06-02 16:22:40 +0800

    remove unsuded comment

--- task-2.js
+++ task-2.js
@@ -1,20 +1,3 @@
-/*
-Buatlah program searching nama yang dapat dibatasi jumlah outputnya yang menerapkan  callback function dengan data sebagai berikut:
-const name = [
-"Abigail", "Alexandra", "Alison",
-"Amanda", "Angela", "Bella",
-"Carol", "Caroline", "Carolyn",
-"Deirdre", "Diana", "Elizabeth",
-"Ella", "Faith", "Olivia", "Penelope"]
-
-Contoh:
-searchName(“an”, 3, callback)
-
-Output: 
-[“Alexandra”,”Amanda”,”Angela”]
-*/
-
-
 const names = [
    "Abigail", "Alexandra", "Alison",
    "Amanda", "Angela", "Bella",
