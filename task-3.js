6fb833ced240a4d27d18937b8ba916ecd8699597
aldoBangun <punk.aldo68@gmail.com>
2022-06-07 08:24:24 +0800

    Add validation (validasiInput) and some example on running the seleksiNilai function

--- task-3.js
+++ task-3.js
@@ -1,11 +1,47 @@
-const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
-   if(nilaiAwal >= nilaiAkhir) {
-      console.log("Nilai akhir harus lebih besar dari nilai awal")
-      return 
+const validasiInput = ({ nilaiAwal, nilaiAkhir, dataArray }) => {
+   const error = { error: true }
+   const validasiNilai = typeof nilaiAwal !== 'number' || typeof nilaiAkhir !== 'number'
+   
+   
+   if(validasiNilai) {
+      error.pesan = 'Nilai awal dan nilai akhir harus bertipe data "number"'
+      return error
+   }
+   
+   if(!Array.isArray(dataArray)) {
+      error.pesan = 'Parameter ke-3 (dataArray) harus berbentuk "array"'
+      return error
    }
 
+   const semuaDataNumber = dataArray.every(data => {
+      return typeof data === 'number'
+   })
+
+   if(!semuaDataNumber) {
+      error.pesan = 'Semua data dalam dataArray harus bertipe data "number"'
+      return error
+   }
+
+   // validasi yang diminta disoal
+   if(nilaiAwal >= nilaiAkhir) {
+      error.pesan = 'Nilai akhir harus lebih besar dari nilai awal'
+      return error
+   }
+
+   // validasi yang diminta disoal
    if(dataArray.length < 5) {
-      console.log("Jumlah angka dalam dataArray harus lebih dari 5")
+      error.pesan = 'Jumlah angka dalam dataArray harus lebih dari 5'
+      return error
+   }
+
+   return { error: false }
+}
+
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+   const adaError = validasiInput({ nilaiAwal, nilaiAkhir, dataArray })
+
+   if(adaError.error) {
+      console.log(adaError.pesan)
       return
    }
 
@@ -21,4 +57,10 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
    }
 }
 
-seleksiNilai(4, 10, [3,2,11,12,45])
+seleksiNilai(4, 10, [3,2,11,12,45]) // Nilai tidak ditemukan
+seleksiNilai('4', 10, [1,2,3,4,5]) // Nilai awal dan nilai akhir harus bertipe data "number"
+seleksiNilai(4, 5, 'Bukan array') // dataArray harus bertipe "Array"
+seleksiNilai(4, 10, [1, 2, 3, 4, '5']) // semua data dalam array harus bertipe "number"
+seleksiNilai(14, 10, [3,2,11,12,45]) // Nilai akhir harus lebih besar dari nilai awal
+seleksiNilai(5, 10, [11, 12, 30]) // Jumlah angka dalam dataArray harus lebih dari 5
+seleksiNilai(3, 10, [1, 3, 5, 7, 9]) // [5, 7, 9]
